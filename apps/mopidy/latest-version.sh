4e2e38097a33909d985ae899e030668cc3659626
Tomáš Bouška <tomas@buvis.net>
2022-06-20 12:40:11 +0200

    Fix mopidy's version getter

--- apps/mopidy/latest-version.sh
+++ apps/mopidy/latest-version.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-version="curl -Ls https://pypi.org/pypi/Mopidy-Iris/json | jq -r .info.version"
+version="$(curl -Ls https://pypi.org/pypi/Mopidy-Iris/json | jq -r .info.version)"
 printf "%s" "${version}"
