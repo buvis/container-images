18a3a75a6bb5e200b615363db2654bd987477e2b
Tomáš Bouška <tomas@buvis.net>
2022-06-20 15:41:19 +0200

    Separate base and app

---
+++ base/debian-slim/README.md
@@ -0,0 +1,8 @@
+## Purpose
+
+This is Debian slim base for my containers
+
+## Hosting
+
+- [Docker Hub](https://hub.docker.com/repository/docker/buvis/debian-slim)
+- [GitHub Container registry](https://ghcr.io/buvis/debian-slim)
---
+++ base/debian-slim/shim/greeting.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "
+Welcome to a buvis container!
+* Repository: https://github.com/buvis/container-images
+* Docs: https://docs.buvis.net
+* This container uses the micro CLI text editor
+* Bugs or feature requests should be opened in an GH issue
+* Inspired by: https://github.com/k8s-at-home/container-images
+"
