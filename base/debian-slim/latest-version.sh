93d4006fb958ef4399e01da859228d5c566342ed
Tomáš Bouška <tomas@buvis.net>
2022-11-09 17:19:52 +0100

    Fix latest debian version determination looks like the sort order of images changed

--- base/debian-slim/latest-version.sh
+++ base/debian-slim/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(curl -s "https://hub.docker.com/v2/repositories/library/debian/tags?page_size=100" | jq --raw-output '.results[] | select(.name | test("^stable-.*-slim$")) | .name'  | tail -n1)
+version=$(curl -s "https://hub.docker.com/v2/repositories/library/debian/tags?page_size=100" | jq --raw-output '.results[] | select(.name | test("^stable-.*-slim$")) | .name'  | head -n1)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
