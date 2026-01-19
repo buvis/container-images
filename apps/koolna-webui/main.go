e7f3bbfb51a1916ae6020d495469e38cda805f12
Tomáš Bouška <tomas@buvis.net>
2026-01-19 09:06:58 +0100

    feat(koolna-webui): implement REST API handlers for Koolna CRUD

--- apps/koolna-webui/main.go
+++ apps/koolna-webui/main.go
@@ -1,32 +1,41 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "net/http"
-    "os"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
+
+	"github.com/buvis/container-images/apps/koolna-webui/handlers"
+	"github.com/buvis/container-images/apps/koolna-webui/k8s"
 )
 
 func main() {
-    port := flag.Int("port", 8080, "Port to run the web UI on")
-    flag.Parse()
+	port := flag.Int("port", 8080, "Port to run the web UI on")
+	flag.Parse()
 
-    router := mux.NewRouter()
-    router.HandleFunc("/health", healthHandler).Methods("GET")
+	client, err := k8s.NewClient()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to initialize kubernetes client: %v\n", err)
+		os.Exit(1)
+	}
+	apiHandler := handlers.NewAPIHandler(client, "")
 
-    // TODO: register API routes for koolna services here
-    // TODO: serve static files for the frontend here
+	router := mux.NewRouter()
+	router.HandleFunc("/health", healthHandler).Methods("GET")
 
-    addr := fmt.Sprintf(":%d", *port)
-    fmt.Fprintf(os.Stdout, "Starting koolna-webui on %s\n", addr)
-    if err := http.ListenAndServe(addr, router); err != nil {
-        fmt.Fprintf(os.Stderr, "server error: %v\n", err)
-        os.Exit(1)
-    }
+	handlers.RegisterRoutes(router, apiHandler)
+
+	addr := fmt.Sprintf(":%d", *port)
+	fmt.Fprintf(os.Stdout, "Starting koolna-webui on %s\n", addr)
+	if err := http.ListenAndServe(addr, router); err != nil {
+		fmt.Fprintf(os.Stderr, "server error: %v\n", err)
+		os.Exit(1)
+	}
 }
 
 func healthHandler(w http.ResponseWriter, _ *http.Request) {
-    w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusOK)
 }
